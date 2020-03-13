ce86cf302ffb405920afea4a9ab29c4e3417226f
daniel <daniel553@github.com>
2020-03-13 12:02:27 -0600

    [Added] package for app

---
+++ src/main/kotlin/com/tripletres/musicgifbox/MyApp.kt
@@ -0,0 +1,30 @@
+package com.tripletres.musicgifbox
+
+import com.tripletres.musicgifbox.view.MyView
+import javafx.stage.Stage
+import javafx.stage.StageStyle
+import tornadofx.App
+import tornadofx.launch
+import kotlin.system.exitProcess
+
+
+/**
+ * Main app that extends from [App], it is the container of any views and user interaction
+ */
+class MyApp : App(MyView::class) {
+
+    /**
+     * Auxiliary function to launch [App]
+     */
+    fun main(args: Array<String>) {
+        launch<MyApp>(args)
+        exitProcess(0)
+    }
+
+    override fun start(stage: Stage) {
+        //For borderless window
+        //stage.initStyle(StageStyle.UNDECORATED)
+        stage.initStyle(StageStyle.DECORATED)
+        super.start(stage)
+    }
+}

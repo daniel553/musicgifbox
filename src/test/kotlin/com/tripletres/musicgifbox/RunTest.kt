9e27a6e4e1163705b6b0bbf48a0cda9168ba163d
daniel <daniel553@github.com>
2020-03-28 11:58:35 -0600

    [Added] junit5 and libraries mockk

---
+++ src/test/kotlin/com/tripletres/musicgifbox/RunTest.kt
@@ -0,0 +1,15 @@
+package com.tripletres.musicgifbox
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class RunTest {
+
+    @Test
+    fun justRun() {
+
+        //TODO: add more test
+        assert(true)
+    }
+}
